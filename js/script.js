3bf44498ce55557040110e00b478fd817ea92c62
Azat <ergeshbaevazat@gmail.com>
2022-01-05 13:07:28 +0600

    cicle

--- js/script.js
+++ js/script.js
@@ -68,6 +68,25 @@
 Проверить, чтобы все работало без ошибок в консоли */
 
 'use strict';
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
+
+// const a = prompt('Один из последних просмотренных фильмов?', ''),
+//       b = prompt('На сколько оцените его?', ''),
+//       c = prompt('Один из последних просмотренных фильмов?', ''),
+//       d = prompt('На сколько оцените его?', '');
+
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
@@ -78,12 +97,89 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+for (let i= 0; i < 2; i++ ) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log( 'error');
+        i--;
+    }
+    
+}
+
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильма");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30 ) {
+    console.log("Вы классный зритель");
+} else if (personalMovieDB.count >= 30 ) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошло ошибка");
+}
 
 console.log(personalMovieDB);
+
+
+
+// if ( 1) {
+//     console.log('Ok!');
+// }
+// else{
+//     console.log('Error');
+// }
+
+// const num = 50;
+
+// if (num < 49) {
+//     console.log('Error');
+// } else if (num > 100) {
+//     console.log('Many');
+// } else {
+//     console.log('OK!');
+// }
+
+// (num === 50) ? console.log('OK!') : console.log('Error');
+
+
+// const num = 50;
+
+// switch (num) {
+//     case 49:
+//         console.log('false');
+//         break;
+//     case 100:
+//         console.log('false');
+//         break;
+//     case 51:
+//         console.log('true');
+//         break;
+//     default:
+//         console.log('not this time');
+//         break;
+// }
+
+// let num = 50;
+
+// while ( num <= 55){
+//     console.log(num);
+//     num++;
+// }
+
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num < 55);
+
+// for (let i = 1; i < 10; i++ ) {
+//     if (i === 6){
+//         // break;
+//         continue;
+//     }
+
+//     console.log(i);
+// }

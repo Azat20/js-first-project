0580cdae1e2cb1c65308c053b3e5796b1bf654f4
Azat <ergeshbaevazat@gmail.com>
2022-01-06 14:39:00 +0600

    add Massiv

--- js/script.js
+++ js/script.js
@@ -277,3 +277,156 @@ writeYourGenres();
 // const test = "12.2px" ;
 // console.log (parseInt(test));
 // console.log(parseFloat(test));
+
+function first () {
+    // do something
+    setTimeout(function(){
+        console.log(1);
+    }, 500);
+}
+
+function second () {
+    console.log (2) ;
+}
+
+first();
+second();
+
+function learnJS (lang, callback){
+    console.log (`Я учу: ${lang}`);
+    callback();
+}
+
+function done() {
+    console.log("Я прошел этот урок!");
+}
+
+learnJS ('Javascript', done);
+
+
+
+const options = {
+    name: 'test',
+    with: 1024,
+    height: 1024,
+    colors: {
+        border:'black', 
+        bg: 'red'
+    },
+    makeTest: function () {
+        console.log("Test");
+    } 
+};
+
+options.makeTest();
+
+const{border,bg} = options.colors;
+console.log(border);
+
+// console.log(Objects.keys(options).length);
+
+// console.log(options["colors"]["border"]);
+
+// delete options.name;
+// console.log(options);
+
+let counter = 0;
+
+for (let key in options){
+    if (typeof(options[key]) === "object") {
+        for (let i in options[key]){
+            console.log(`Свойства ${i} имеет значение ${ options[key][i]}`);
+            counter++;
+        }
+    } else {
+        console.log(`Свойства ${key} имеет значение ${ options[key]}`);
+        counter++;
+    }
+}
+
+console.log(counter);
+
+
+
+const arr = [1, 42, 23, 16, 8];
+arr.sort(compaerNum);
+console.log(arr);
+
+function compaerNum (a,b){
+    return a - b;
+}
+
+// arr [99] = 0;
+// console.log(arr.length);
+// console.log(arr);
+
+arr.forEach(function(item, i, arr) {
+    console.log(`${i}: ${item} внутри массива ${arr}`);
+});
+
+// arr.pop();
+// arr.push(10);
+
+// console.log(arr);
+
+// for (let i = 0; i < arr.length; i++){
+//     console.log(arr[i]);
+// }
+
+for (let value of arr) {
+    console.log(value);
+}
+
+const str = prompt("", "");
+const products = str.split ("","");
+products.sort();
+console.log(products.joins(';'));
+
+
+
+// let a = 5,
+//     b = a;
+
+// b = b + 5;
+
+// console.log(b);
+// console.log(a);
+
+// const obj = {
+//     a: 5,
+//     b: 1
+// };
+// const copy = obj;
+
+// copy.a = 10;
+
+// console.log(copy);
+// console.log(obj);
+
+function copy(mainObj) {
+    let objCopy = {};
+
+    let key;
+    for(key in mainObj){
+        objCopy[key] = mainObj[key];
+    }
+
+    return objCopy;
+}
+
+const numbers = {
+    a: 2,
+    b: 5,
+    c: {
+        x: 7, 
+        y: 4
+    }
+};
+
+const newNumbers = copy(numbers);
+
+newNumbers.a =10 ;
+newNumbers.c.x = 10;
+
+console.log(newNumbers);
+console.log(numbers);
